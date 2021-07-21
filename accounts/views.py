552ffc3c25b7ce6b0c4a3726cad9d77c0f20a903
m7mdGNo <m7mdelganainy2001@gmail.com>
2021-07-21 07:26:56 +0100

    'otp'

--- accounts/views.py
+++ accounts/views.py
@@ -108,7 +108,7 @@ def error_page(request):
 
 def send_mail_after_registration(email , token):
     subject = 'Your accounts need to be verified'
-    message = f'Hi paste the link to verify your account https://websitegno.herokuapp.com/{token}'
+    message = f'Hi paste the link to verify your account https://websitegno.herokuapp.com/verify/{token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message , email_from ,recipient_list )
